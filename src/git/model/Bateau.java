5beb7f1d6f162848e2864377f27ebd03558f9cf2
wakko <10800651@F207-4>
2014-02-10 10:57:52 +0100

    Added Bateau class

---
+++ src/git/model/Bateau.java
@@ -0,0 +1,37 @@
+package git.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bateau {
+	private Long id;
+	
+	private String nom;
+
+	private List<Marin> equipage = new ArrayList<Marin>();
+	
+	public Long getId() {
+		return id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public List<Marin> getEquipage() {
+		return new ArrayList<Marin>(this.equipage);
+	}
+
+	public boolean setMarin(Marin m) {
+		if(m != null)
+		{
+			return equipage.add(m);
+		}
+		return false;
+	}	
+}
