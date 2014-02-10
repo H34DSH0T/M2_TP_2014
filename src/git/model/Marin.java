f21638a6ac01127cfba7725481cdf3c8033611a0
wakko <10800651@F207-4>
2014-02-10 10:56:05 +0100

    Added Marin class

---
+++ src/git/model/Marin.java
@@ -0,0 +1,68 @@
+package git.model;
+
+public class Marin {
+/******** CHAMPS ********/
+	protected Long id;
+
+	private String nom;
+	private String prenom;
+	private int age;
+
+	/********** CONSTRUCTEURS *********/
+	public Marin(){
+		return;
+	}
+	
+	public Marin(String nom, String prenom, int age) {
+		//super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.age = age;
+	}
+
+	/********* GETTERS *********/
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	/********* SETTERS *********/
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	/******* METHODES ******/
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("id=").append(id+", ");
+		sb.append("nom=").append(nom+", ");
+		sb.append("prenom=").append(prenom+", ");
+		sb.append("age=").append(age);
+		return sb.toString();
+	}
+}
